906f89bdd7238cab29f6192c3fe29f765102529d
Vladimir Sukanica <vsukanica@vladimirs-imac.local>
2018-04-05 11:05:37 +0200

    Da se naspavam

--- GitIgranje/ViewController.swift
+++ GitIgranje/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Cekalo je jutro, celo jutro da se naspavam")
     }
 
     override func didReceiveMemoryWarning() {
